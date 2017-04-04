3933d98253e396fddb86808a9869083fa1dc6063
Francesco De Carlo <decarlof@gmail.com>
2017-04-03 20:50:31 -0500

    first commit

--- fly_scan.py
+++ fly_scan.py
@@ -11,49 +11,62 @@ import os
 import numpy as np
 
 
-if __name__ == '__main__':
-
-    exposure_time          = 0.1             # s
-    angular_range          = 180.0           # deg
-    readout_time           = 0.000           # s
-    blur_error             = 0.159            # pixel
-    blur_error             = 0.1            # pixel
-
-    camera_size_x          = 2048            # pixel
-    number_of_proj         = 180
-
-    min_scan_time = number_of_proj * (exposure_time + readout_time)
-    max_rot_speed = angular_range / min_scan_time
-    max_frame_rate = number_of_proj / min_scan_time
-
-    max_delta_blur = np.arccos(((camera_size_x/2.0) - blur_error) / (camera_size_x/2.0)) * 180.0 / np.pi
-    max_rot_speed_blur = max_delta_blur / exposure_time
+def blur_error(exposure_time, readout_time, camera_size_x, angular_range, number_of_proj):
 
     angular_step = angular_range/number_of_proj
- 
-    blur_delta = exposure_time * max_rot_speed
+    scan_time = number_of_proj * (exposure_time + readout_time)
+    rot_speed = angular_range / scan_time
+    frame_rate = number_of_proj / scan_time
+    blur_delta = exposure_time * rot_speed
     blur_pixel = (camera_size_x / 2.0) - ((camera_size_x / 2.0) * np.cos(blur_delta * np.pi /180.))
 
-    min_scan_time_blur = angular_range / max_rot_speed_blur 
-#    max_frame_rate_blur = number_of_proj / min_scan_time_blur
-    max_frame_rate_blur = number_of_proj / min_scan_time_blur
-
-    print("Exposure Time: ", exposure_time, "s")
+    print("*************************************")
     print("Total # of proj: ", number_of_proj)
+    print("Exposure Time: ", exposure_time, "s")
+    print("Readout Time: ", readout_time, "s")
     print("Angular Range: ", angular_range, "degrees")
-
+    print("Camera X size: ", camera_size_x)
+    print("*************************************")
     print("Angular Step: ", angular_step, "degrees")   
+    print("Scan Time: ", scan_time ,"s") 
+    print("Rot Speed: ", rot_speed, "degrees/s")
+    print("Frame Rate: ", frame_rate, "fps")
+    print("Blur: ", blur_pixel, "pixels")
+    print("*************************************")
+    
+    return blur_error
 
-    print("Max Rot Speed: ", max_rot_speed, "degrees/s")
-    print("Min Scan Time: ", min_scan_time ,"s") 
-    print("Max Frame Rate: ", max_frame_rate, "fps")
+def frame_rate(blur_error, exposure_time, readout_time, camera_size_x, angular_range, number_of_proj):
 
-    print("Set Blur Error: ", blur_error, "pixel")
-    print("Max Rot Speed before blurring: ", max_rot_speed_blur, "degrees/s")
-    print("Min Scan Time before blurring: ", min_scan_time_blur, "s")
-    print("Max Frame Rate before blurring: ", max_frame_rate_blur, "fps")
+    delta_blur  = np.arccos(((camera_size_x / 2.0) - blur_error) / (camera_size_x / 2.0)) * 180.0 / np.pi
+    rot_speed = delta_blur  / exposure_time
 
-    #print("Blur delta: ", blur_delta)
-    #print("Blur pixel: ", blur_pixel)
-    #print("Blur delta max: ", max_delta_blur)
- 
+    scan_time = angular_range / rot_speed
+    frame_rate = number_of_proj / scan_time
+    print("*************************************")
+    print("Total # of proj: ", number_of_proj)
+    print("Exposure Time: ", exposure_time, "s")
+    print("Readout Time: ", readout_time, "s")
+    print("Angular Range: ", angular_range, "degrees")
+    print("Camera X size: ", camera_size_x)
+    print("Blur Error: ", blur_error, "pixels")
+    print("*************************************")
+    print("Rot Speed: : ", rot_speed, "degrees/s")
+    print("Scan Time:: ", scan_time, "s")
+    print("Frame Rate: ", frame_rate, "fps")
+    print("*************************************")
+  
+    return frame_rate
+
+if __name__ == '__main__':
+
+    exposure_time          = 0.4             # s
+    angular_range          = 180.0           # deg
+    readout_time           = 0.1             # s
+    camera_size_x          = 2048            # pixel
+    number_of_proj         = 1500
+
+    blur_error = blur_error(exposure_time, readout_time, camera_size_x, angular_range, number_of_proj)
+
+    blur_error = 0.00143736498376        # pixel
+    frame_rate = frame_rate(blur_error, exposure_time, readout_time, camera_size_x, angular_range, number_of_proj)    
